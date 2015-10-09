9177fb04bf9ae9d3c68b9c02c2f303f4dd356fb7
aspin <tsunami70875@gmail.com>
2015-10-09 11:23:52 -0500

    inital commit

---
+++ client/views/home.js
@@ -0,0 +1,11 @@
+/*
+
+    ADD IN JAVASCRIPT CODE HERE.
+
+    Rename and reorder these pages as necessary.
+
+*/
+
+Template.home.onRendered = function () {
+    // add javascript to be executed when the template first_view is rendered
+};
