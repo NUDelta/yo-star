7106e57842b91ff2d2be9e91294f150a52fdc41b
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2015-10-13 21:31:02 -0500

    Begin basic lobby system

---
+++ collections/collections.js
@@ -0,0 +1,7 @@
+Lobbies = new Mongo.Collection("lobbys");
+Games = new Mongo.Collection("games");
+
+// Accounts.onCreateUser(function(options, user) {
+//   user.inLobby = false;
+//   return user;
+// });
--- server/methods.js
+++ server/methods.js
@@ -3,12 +3,32 @@
     ADD IN SERVER METHODS HERE.
 
     e.g. if you need to call a secure function from the
-    server that has full access to database.
+    serzer that has full access to database.
 
 */
 
 Meteor.methods({
-    methodName: function () {
-        // ...
+	createLobby: function() {
+		// Lobbies.remove({});
+		Lobbies.insert({
+			users: [],
+			active: false,
+			createdAt: new Date()
+		});
+	},
+    joinLobby: function(user) {
+        if (Lobbies.find().count() === 0) {
+        	Meteor.call("createLobby");
+        }
+        var currLobby = Lobbies.findOne({active: false});
+        Lobbies.update({ _id: Meteor.user().profile.lobby._id },{ $push: { "users": Meteor.user().username }});
+        Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.isInLobby":true}})
+        Meteor.users.update({_id:Meteor.user()._id}, {$set: {"profile.lobby": currLobby}});
+        console.log('Lobby joined');
+        console.log(user);
+    },
+    leaveLobby: function(user) {
+    	Meteor.users.update({_id:Meteor.user()._id}, {$set: {"profile.isInLobby": false}});
+    	console.log('Lobby left');
     }
 });
