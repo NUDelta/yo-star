cd11d5c379987ebb6ba81bd0d15659c3fbcb2bf3
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2015-10-16 19:15:32 -0500

    Added score to live view

--- client/views/live.js
+++ client/views/live.js
@@ -59,5 +59,49 @@ Template.live.helpers({
                 zoom: MAP_ZOOM
             };
         }
+    },
+    score: function() {
+        var userLats = [];
+        var userLngs = [];
+        if (Meteor.user()) {
+            Meteor.users.find({ lobby: Meteor.user().lobby }).forEach(function(user) {
+
+                userLats.push(user.profile.location.lat);
+                userLngs.push(user.profile.location.lng);
+            });
+            var lr = linearRegression(userLngs, userLats);
+            console.log("UserLats: " + userLats);
+            console.log("UserLngs: " + userLngs);
+            console.log("R-squared: " + lr.r2);
+            return lr.r2? lr.r2 : 0;
+        }
+        else {
+            return 0;
+        }
     }
 });
+
+function linearRegression(x, y){
+        var lr = {};
+        var n = y.length;
+        var sum_x = 0;
+        var sum_y = 0;
+        var sum_xy = 0;
+        var sum_xx = 0;
+        var sum_yy = 0;
+        
+        for (var i = 0; i < y.length; i++) {
+            
+            sum_x += x[i];
+            sum_y += y[i];
+            sum_xy += (x[i]*y[i]);
+            sum_xx += (x[i]*x[i]);
+            sum_yy += (y[i]*y[i]);
+        } 
+        
+        lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
+        lr['intercept'] = (sum_y - lr.slope * sum_x)/n;
+        lr['r2'] = Math.pow((n*sum_xy - sum_x*sum_y)/Math.sqrt((n*sum_xx-sum_x*sum_x)*(n*sum_yy-sum_y*sum_y)),2);
+        
+        return lr;
+}
