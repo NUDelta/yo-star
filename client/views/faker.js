d9301daba3887bcdd0e5eb8caa3478c37565e7a0
aspin <tsunami70875@gmail.com>
2015-10-21 22:57:58 -0500

    Added page to manage fake user locations for testing purposes.

---
+++ client/views/faker.js
@@ -0,0 +1,48 @@
+Template.faker.helpers({
+    lobbies: function() {
+        return Lobbies.find();
+    },
+    getUser: function(username) {
+        return Meteor.users.findOne({ username: username });
+    },
+    fakeUsers: function() {
+        return Meteor.users.find({ 'profile.fake': true, 'profile.lobby': this._id});
+    }
+});
+
+Template.faker.events({
+    'keyup form': function(event, template) {
+        event.preventDefault();
+        let userId = event.currentTarget.getAttribute('user'),
+            lat = parseFloat(event.currentTarget.lat.value),
+            lng = parseFloat(event.currentTarget.lng.value),
+            location = { lat: lat, lng: lng };
+
+        if (lat && lng) {
+            Meteor.users.update(userId, { $set: { 'profile.location': location } });
+        }
+    },
+    'submit form': function(event, template) {
+        event.preventDefault();
+    },
+    'click #makeFake': function(event, template) {
+        let randomNum = Math.round(Math.random() * 10000),
+            lobby = event.target.getAttribute('lobby'),
+            userObject = {
+                username: `test${randomNum}`,
+                password: 'password',
+                profile: {
+                    lobby: lobby,
+                    isInLobby: true,
+                    fake: true,
+                }
+            };
+        Accounts.createUser(userObject, (err) => {
+            Meteor.logout();
+        });
+    },
+    'click #deleteFake': function(event, template) {
+        let userId = event.target.getAttribute('user');
+        Meteor.users.remove(userId);
+    }
+});
