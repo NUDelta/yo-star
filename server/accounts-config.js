aeee937f90da6e9c1381959e582123a8119da6b4
aspin <tsunami70875@gmail.com>
2015-10-21 22:57:58 -0500

    Added page to manage fake user locations for testing purposes.

--- server/accounts-config.js
+++ server/accounts-config.js
@@ -1,6 +1,8 @@
 Accounts.onCreateUser(function(options, user) {
+    console.log(options);
     user.profile = options.profile || {};
-    user.profile.location = 0;
+    user.profile.location = user.profile.location || { lat: 0, lng: 0 };
+    user.profile.isInLobby = user.profile.isInLobby || false;
     return user;
 });
 
@@ -9,7 +11,7 @@ Meteor.users.find({ 'status.online': true }).observeChanges({
     added: function(id, fields) {
         let user = Meteor.users.findOne(id)
         let currentLobby = Lobbies.findOne(user.profile.lobby);
-        if (currentLobby) {
+        if (currentLobby && !user.profile.fake) {
             console.log(`[status] ${user.username} just came online and entered ${currentLobby._id}`);
             Meteor.users.update(id, { $set: { 'profile.isInLobby': true } });
             Lobbies.update(currentLobby._id, { $push: { 'users': user.username } });
@@ -18,10 +20,23 @@ Meteor.users.find({ 'status.online': true }).observeChanges({
     removed: function(id) {
         let user = Meteor.users.findOne(id)
         let currentLobby = Lobbies.findOne(user.profile.lobby);
-        if (currentLobby) {
+        if (currentLobby && !user.profile.fake) {
             console.log(`[status] ${user.username} just went offline and left ${currentLobby._id}`);
             Meteor.users.update(id, { $set: { 'profile.isInLobby': false } });
             Lobbies.update(currentLobby._id, { $pull: { 'users': user.username } });
         }
+    }
+});
+
+// DEBUG ONLY
+Meteor.users.allow({ 
+    insert: function() {
+        return true;
+    },
+    update: function() {
+        return true;
+    },
+    remove: function() {
+        return true;
     }
 });
